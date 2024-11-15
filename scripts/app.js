e9fd24bcb178d132321b87f115637af2ade6a65b
Mohammad Noohi <noohi.m98@gmail.com>
2024-11-15 18:34:21 +0330

    implement menu section

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,29 @@
 "use strict";
 
+let mouseDown = false;
+let startX, scrollLeft;
+const menuFilterList = document.querySelector('.menu__filter-list');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - menuFilterList.offsetLeft;
+  scrollLeft = menuFilterList.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - menuFilterList.offsetLeft;
+  const scroll = x - startX;
+  menuFilterList.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+menuFilterList.addEventListener('mousemove', move, false);
+menuFilterList.addEventListener('mousedown', startDragging, false);
+menuFilterList.addEventListener('mouseup', stopDragging, false);
+menuFilterList.addEventListener('mouseleave', stopDragging, false);
